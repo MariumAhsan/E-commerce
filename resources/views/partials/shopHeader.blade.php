55fdc78383a43262dc24934f34d3c577f2168deb
MariumAhsan <ahsanmarium99@gmail.com>
2024-04-15 14:16:51 +0600

    more edits

--- resources/views/partials/shopHeader.blade.php
+++ resources/views/partials/shopHeader.blade.php
@@ -101,8 +101,7 @@
                         <ul>
                             <li><a href="">About Us</a></li>
                             <li><a href="">My Account</a></li>
-                            <li><a href="">Wishlist</a></li>
-                            <li><a href="">Order Tracking</a></li>
+                            
                         </ul>
                     </div>
                 </div>
@@ -110,9 +109,9 @@
                     <div class="text-center">
                         <div id="news-flash" class="d-inline-block">
                             <ul>
-                                <li>100% Secure delivery without contacting the courier</li>
-                                <li>Supper Value Deals - Save more with coupons</li>
-                                <li>Trendy 25silver jewelry, save up 35% off today</li>
+                                <li>100% Secure delivery</li>
+                                <li>Super Value Deals - Save more with coupons</li>
+                                <li>Unlimited Discounts, save upto 35-50% off !!!</li>
                             </ul>
                         </div>
                     </div>
@@ -120,32 +119,22 @@
                 <div class="col-xl-3 col-lg-4">
                     <div class="header-info header-info-right">
                         <ul>
-                            <li>Need help? Call Us: <strong class="text-brand"> + 1800 900</strong></li>
+                            <li>Need help? Call Us: <strong class="text-brand">+880-38881</strong></li>
                             <li>
-                                <a class="language-dropdown-active" href="#">English <i class="fi-rs-angle-small-down"></i></a>
-                                <ul class="language-dropdown">
-                                    <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-fr.png" alt="" />Français</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-dt.png" alt="" />Deutsch</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-ru.png" alt="" />Pусский</a>
-                                    </li>
-                                </ul>
+                                <a class="language-dropdown-active" href="#">English </a>
+                                
                             </li>
                             <li>
-                                <a class="language-dropdown-active" href="#">USD <i class="fi-rs-angle-small-down"></i></a>
+                                <a class="language-dropdown-active" href="#">Payment Method <i class="fi-rs-angle-small-down"></i></a>
                                 <ul class="language-dropdown">
                                     <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-fr.png" alt="" />INR</a>
+                                        <a href="#">COD</a>
                                     </li>
                                     <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-dt.png" alt="" />MBP</a>
+                                        <a href="#">Bkash</a>
                                     </li>
                                     <li>
-                                        <a href="#"><img src="{{asset('assets')}}/assets/imgs/theme/flag-ru.png" alt="" />EU</a>
+                                        <a href="#">Cards</a>
                                     </li>
                                 </ul>
                             </li>
@@ -173,33 +162,6 @@
                     </div>
                     <div class="header-action-right">
                         <div class="header-action-2">
-                            <div class="search-location">
-                                <form action="#">
-                                    <select class="select-active">
-                                        <option>Your Location</option>
-                                        <option>Alabama</option>
-                                        <option>Alaska</option>
-                                        <option>Arizona</option>
-                                        <option>Delaware</option>
-                                        <option>Florida</option>
-                                        <option>Georgia</option>
-                                        <option>Hawaii</option>
-                                        <option>Indiana</option>
-                                        <option>Maryland</option>
-                                        <option>Nevada</option>
-                                        <option>New Jersey</option>
-                                        <option>New Mexico</option>
-                                        <option>New York</option>
-                                    </select>
-                                </form>
-                            </div>
-                            <div class="header-action-icon-2">
-                                <a href="shop-compare.html">
-                                    <img class="svgInject" alt="Nest" src="{{asset('assets')}}/assets/imgs/theme/icons/icon-compare.svg" />
-                                    <span class="pro-count blue">3</span>
-                                </a>
-                                <a href="shop-compare.html"><span class="lable ml-0">Compare</span></a>
-                            </div>
                             <div class="header-action-icon-2">
                                 <a href="shop-wishlist.html">
                                     <img class="svgInject" alt="Nest" src="{{asset('assets')}}/assets/imgs/theme/icons/icon-heart.svg" />
@@ -376,7 +338,7 @@
                             <ul>
                                 <li class="hot-deals"><img src="{{asset('assets')}}/assets/imgs/theme/icons/icon-hot.svg" alt="hot deals" /><a href="shop-grid-right.html">Deals</a></li>
                                 <li>
-                                    <a class="active" href="/">Home <i class="fi-rs-angle-down"></i></a>
+                                    <a class="active" href="/">Home </a>
                                     <ul class="sub-menu">
                                         <li><a href="index.html">Home 1</a></li>
                                         <li><a href="index-2.html">Home 2</a></li>
@@ -390,50 +352,8 @@
                                     <a href="">About</a> <!--Make an about us page--->
                                 </li>
                                 <li>
-                                    <a href="{{route('pages.shop-grid-left')}}">Shop <i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="shop-grid-right.html">Shop Grid – Right Sidebar</a></li>
-                                        <li><a href="shop-grid-left.html">Shop Grid – Left Sidebar</a></li>
-                                        <li><a href="shop-list-right.html">Shop List – Right Sidebar</a></li>
-                                        <li><a href="shop-list-left.html">Shop List – Left Sidebar</a></li>
-                                        <li><a href="shop-fullwidth.html">Shop - Wide</a></li>
-                                        <li>
-                                            <a href="#">Single Product <i class="fi-rs-angle-right"></i></a>
-                                            <ul class="level-menu">
-                                                <li><a href="shop-product-right.html">Product – Right Sidebar</a></li>
-                                                <li><a href="shop-product-left.html">Product – Left Sidebar</a></li>
-                                                <li><a href="shop-product-full.html">Product – No sidebar</a></li>
-                                                <li><a href="shop-product-vendor.html">Product – Vendor Info</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="shop-filter.html">Shop – Filter</a></li>
-                                        <li><a href="shop-wishlist.html">Shop – Wishlist</a></li>
-                                        <li><a href="shop-cart.html">Shop – Cart</a></li>
-                                        <li><a href="shop-checkout.html">Shop – Checkout</a></li>
-                                        <li><a href="shop-compare.html">Shop – Compare</a></li>
-                                        <li>
-                                            <a href="#">Shop Invoice<i class="fi-rs-angle-right"></i></a>
-                                            <ul class="level-menu">
-                                                <li><a href="shop-invoice-1.html">Shop Invoice 1</a></li>
-                                                <li><a href="shop-invoice-2.html">Shop Invoice 2</a></li>
-                                                <li><a href="shop-invoice-3.html">Shop Invoice 3</a></li>
-                                                <li><a href="shop-invoice-4.html">Shop Invoice 4</a></li>
-                                                <li><a href="shop-invoice-5.html">Shop Invoice 5</a></li>
-                                                <li><a href="shop-invoice-6.html">Shop Invoice 6</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Vendors <i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="vendors-grid.html">Vendors Grid</a></li>
-                                        <li><a href="vendors-list.html">Vendors List</a></li>
-                                        <li><a href="vendor-details-1.html">Vendor Details 01</a></li>
-                                        <li><a href="vendor-details-2.html">Vendor Details 02</a></li>
-                                        <li><a href="vendor-dashboard.html">Vendor Dashboard</a></li>
-                                        <li><a href="vendor-guide.html">Vendor Guide</a></li>
-                                    </ul>
+                                    <a href="{{route('pages.shop-grid-left')}}">Shop </a>
+                                    
                                 </li>
                                 <li class="position-static">
                                     <a href="#">Mega menu <i class="fi-rs-angle-down"></i></a> <!---POPULATE MEGAMENU---->
@@ -497,49 +417,13 @@
                                         </li>
                                     </ul>
                                 </li>
-                                <li>
-                                    <a href="blog-category-grid.html">Blog <i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-category-grid.html">Blog Category Grid</a></li>
-                                        <li><a href="blog-category-list.html">Blog Category List</a></li>
-                                        <li><a href="blog-category-big.html">Blog Category Big</a></li>
-                                        <li><a href="blog-category-fullwidth.html">Blog Category Wide</a></li>
-                                        <li>
-                                            <a href="#">Single Post <i class="fi-rs-angle-right"></i></a>
-                                            <ul class="level-menu level-menu-modify">
-                                                <li><a href="blog-post-left.html">Left Sidebar</a></li>
-                                                <li><a href="blog-post-right.html">Right Sidebar</a></li>
-                                                <li><a href="blog-post-fullwidth.html">No Sidebar</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Pages <i class="fi-rs-angle-down"></i></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="page-about.html">About Us</a></li>
-                                        <li><a href="page-contact.html">Contact</a></li>
-                                        <li><a href="page-account.html">My Account</a></li>
-                                        <li><a href="page-login.html">Login</a></li>
-                                        <li><a href="page-register.html">Register</a></li>
-                                        <li><a href="page-forgot-password.html">Forgot password</a></li>
-                                        <li><a href="page-reset-password.html">Reset password</a></li>
-                                        <li><a href="page-purchase-guide.html">Purchase Guide</a></li>
-                                        <li><a href="page-privacy-policy.html">Privacy Policy</a></li>
-                                        <li><a href="page-terms.html">Terms of Service</a></li>
-                                        <li><a href="page-404.html">404 Page</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="page-contact.html">Contact</a>
-                                </li>
                             </ul>
                         </nav>
                     </div>
                 </div>
                 <div class="hotline d-none d-lg-flex">
                     <img src="{{asset('assets')}}/assets/imgs/theme/icons/icon-headphone.svg" alt="hotline" />
-                    <p>1900 - 888<span>24/7 Support Center</span></p>
+                    <p>+880-38881<span>Emmergency Call</span></p>
                 </div>
                 <div class="header-action-icon-2 d-block d-lg-none">
                     <div class="burger-icon burger-icon-white">
